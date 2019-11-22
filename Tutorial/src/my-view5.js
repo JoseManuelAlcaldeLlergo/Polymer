d9898f33ea8eab682fc12367d268291a2cd39bab
Jose Manuel Alcalde Llergo <43375306+JoseManuelAlcaldeLlergo@users.noreply.github.com>
2019-11-22 18:55:22 +0100

    Add files via upload

---
+++ Tutorial/src/my-view5.js
@@ -0,0 +1,31 @@
+import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
+import './shared-styles.js';
+
+class MyView5 extends PolymerElement {
+  static get template() {
+    return html`
+      
+      <style include="shared-styles">
+        :host {
+          display: block;
+
+          padding: 10px;
+        }
+
+       .card h1{
+          font-size: 3em;
+        }
+
+      </style>
+
+      <div class="card">
+        <div class="circle">5</div>
+        <h1>Creando a partir del polymer starter kit</h1>
+        <wc-markdown src="./Tutorial.md"></wc-markdown>
+      </div>
+
+    `;
+  }
+}
+
+window.customElements.define('my-view5', MyView5);
