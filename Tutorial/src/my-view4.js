74329521e0923424cdc76801e1872420fa1edc77
Jose Manuel Alcalde Llergo <43375306+JoseManuelAlcaldeLlergo@users.noreply.github.com>
2019-11-23 13:09:02 +0100

    Update my-view4.js

--- Tutorial/src/my-view4.js
+++ Tutorial/src/my-view4.js
@@ -46,7 +46,7 @@ class MyView4 extends PolymerElement {
         <p>Estudiante de Ingenier&iacute;a Inform&aacute;tica en la Universidad de C&oacute;rdoba (UCO). </p>
         <div id="redes">
           <p><img src="./src/img/correo.png" id="icono">  i72feurt@uco.es</p>
-          <p><img src="./src/img/github.png" id="icono">  TomasFdez5</p>
+          <p><img src="./src/img/github.png" id="icono">  <a href="https://github.com/TomasFdez5">TomasFdez5</p>
           <p><img src="./src/img/insta.png" id="icono">  tomasfdeez</p>
         </div>
 
